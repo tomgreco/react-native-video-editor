65684c4718bdd321fe8cbcd63ac2ab9e08ebd3d7
tomgreco <tom.greco@scanifly.com>
2021-11-30 11:42:22 -0600

    Native module changes for iOS

--- ios/RNVideoEditor.h
+++ ios/RNVideoEditor.h
@@ -1,9 +1,12 @@
 
-#import "RCTBridgeModule.h"
+#ifndef RNVideoEditor_h
+#define RNVideoEditor_h
+
+#import <React/RCTBridgeModule.h>
 #import <AVFoundation/AVFoundation.h>
 #import <AssetsLibrary/AssetsLibrary.h>
 
 @interface RNVideoEditor : NSObject <RCTBridgeModule>
-
 @end
-  
+
+#endif /* RNVideoEditor_h */
